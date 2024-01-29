7e23bbf874324d99abf3787c08a2984c302a1f20
jbytecode <mhsatman@gmail.com>
2024-01-29 15:36:51 +0300

    add citations

--- test/testtopsis.jl
+++ test/testtopsis.jl
@@ -1,145 +1,155 @@
 @testset "Topsis" begin
 
-    @testset "Preparing decision matrix using multiple decision makers" begin
-        # Two decision makers
-        # Two alternatives 
-        # Four criteria
-        dm1 = [
-            Triangular(3, 5, 7) Triangular(7, 9, 9) Triangular(1, 3, 5) Triangular(3, 5, 7);
-            Triangular(5, 7, 9) Triangular(5, 7, 9) Triangular(1, 3, 5) Triangular(1, 3, 5)
-        ]
+    @testset "Sample in the short paper" begin
 
-        dm2 = [
-            Triangular(3, 5, 7) Triangular(7, 9, 9) Triangular(3, 5, 7) Triangular(3, 5, 7);
-            Triangular(5, 7, 9) Triangular(7, 9, 9) Triangular(1, 3, 5) Triangular(1, 3, 5)
-        ]
+        """
+        References:
 
-        decmat = fuzzydecmat([dm1, dm2])
+        Kore, N. B., Ravi, K., & Patil, S. B. (2017). A simplified description of fuzzy TOPSIS 
+        method for multi criteria decision making. International Research Journal of Engineering
+        and Technology (IRJET), 4(5), 2047-2050.
+        """
 
-        expected = [
-            Triangular(3, 5, 7) Triangular(7, 9, 9) Triangular(1, 4, 7) Triangular(3, 5, 7);
-            Triangular(5, 7, 9) Triangular(5, 8, 9) Triangular(1, 3, 5) Triangular(1, 3, 5)
-        ]
+        @testset "Preparing decision matrix using multiple decision makers" begin
+            # Two decision makers
+            # Two alternatives 
+            # Four criteria
+            dm1 = [
+                Triangular(3, 5, 7) Triangular(7, 9, 9) Triangular(1, 3, 5) Triangular(3, 5, 7);
+                Triangular(5, 7, 9) Triangular(5, 7, 9) Triangular(1, 3, 5) Triangular(1, 3, 5)
+            ]
 
-        n, p = decmat |> size
+            dm2 = [
+                Triangular(3, 5, 7) Triangular(7, 9, 9) Triangular(3, 5, 7) Triangular(3, 5, 7);
+                Triangular(5, 7, 9) Triangular(7, 9, 9) Triangular(1, 3, 5) Triangular(1, 3, 5)
+            ]
 
-        for i in 1:n
-            for j in 1:p
-                @test decmat[i, j] == expected[i, j]
-            end
-        end
-    end
+            decmat = fuzzydecmat([dm1, dm2])
 
-    @testset "Preparing weight vector using multiple decision makers" begin
-        # These are decision makers' weight vectors
-        # There are 2 decision makers.
-        weights = [
-            [
-                Triangular(5, 7, 9),
-                Triangular(7, 9, 9),
-                Triangular(7, 9, 9),
-                Triangular(3, 5, 7),
-            ],
-            [
-                Triangular(3, 5, 7),
-                Triangular(5, 7, 9),
-                Triangular(5, 7, 9),
-                Triangular(1, 3, 5),
-            ],
-        ]
+            expected = [
+                Triangular(3, 5, 7) Triangular(7, 9, 9) Triangular(1, 4, 7) Triangular(3, 5, 7);
+                Triangular(5, 7, 9) Triangular(5, 8, 9) Triangular(1, 3, 5) Triangular(1, 3, 5)
+            ]
 
-        summarizedweights = prepare_weights(weights)
+            n, p = decmat |> size
 
-        expected = [
-            Triangular(3.000, 6.000, 9.000),
-            Triangular(5.000, 8.000, 9.000),
-            Triangular(5.000, 8.000, 9.000),
-            Triangular(1.000, 4.000, 7.000),
-        ]
-
-        for i in eachindex(expected)
-            @test expected[i] == summarizedweights[i]
-        end
-    end
-
-    @testset "Triangular: Alternative: 2, Criteria: 4" begin
-
-        eps = 0.01
-
-        decmat = [
-            Triangular(3, 5, 7) Triangular(7, 9, 9) Triangular(1, 4, 7) Triangular(3, 5, 7)
-            Triangular(5, 7, 9) Triangular(5, 8, 9) Triangular(1, 3, 5) Triangular(1, 3, 5)
-        ]
-
-        n, p = size(decmat)
-
-        w = [
-            Triangular(3, 6, 9),
-            Triangular(5, 8, 9),
-            Triangular(5, 8, 9),
-            Triangular(1, 4, 7),
-        ]
-
-        fns = [minimum, maximum, maximum, maximum]
-
-        result = fuzzytopsis(decmat, w, fns)
-
-        expected_normalized_mat = [
-            Triangular(0.429, 0.600, 1.000) Triangular(0.778, 1.000, 1.000) Triangular(0.143, 0.571, 1.000) Triangular(0.429, 0.714, 1.000)
-            Triangular(0.333, 0.429, 0.600) Triangular(0.556, 0.889, 1.000) Triangular(0.143, 0.429, 0.714) Triangular(0.143, 0.429, 0.714)
-        ]
-
-        for i = 1:n
-            for j = 1:p
-                @test isapprox(
-                    result.normalized_decmat[i, j],
-                    expected_normalized_mat[i, j],
-                    atol=eps,
-                )
+            for i in 1:n
+                for j in 1:p
+                    @test decmat[i, j] == expected[i, j]
+                end
             end
         end
 
-        expected_weighted_norm = [
-            Triangular(1.287, 3.600, 9.000) Triangular(3.890, 8.000, 9.000) Triangular(0.715, 4.568, 9.000) Triangular(0.429, 2.856, 7.000)
-            Triangular(0.999, 2.574, 5.400) Triangular(2.780, 7.112, 9.000) Triangular(0.715, 3.432, 6.426) Triangular(0.143, 1.716, 4.998)
-        ]
+        @testset "Preparing weight vector using multiple decision makers" begin
+            # These are decision makers' weight vectors
+            # There are 2 decision makers.
+            weights = [
+                [
+                    Triangular(5, 7, 9),
+                    Triangular(7, 9, 9),
+                    Triangular(7, 9, 9),
+                    Triangular(3, 5, 7),
+                ],
+                [
+                    Triangular(3, 5, 7),
+                    Triangular(5, 7, 9),
+                    Triangular(5, 7, 9),
+                    Triangular(1, 3, 5),
+                ],
+            ]
 
-        for i = 1:n
-            for j = 1:p
-                @test isapprox(
-                    result.weighted_normalized_decmat[i, j],
-                    expected_weighted_norm[i, j],
-                    atol=eps,
-                )
+            summarizedweights = prepare_weights(weights)
+
+            expected = [
+                Triangular(3.000, 6.000, 9.000),
+                Triangular(5.000, 8.000, 9.000),
+                Triangular(5.000, 8.000, 9.000),
+                Triangular(1.000, 4.000, 7.000),
+            ]
+
+            for i in eachindex(expected)
+                @test expected[i] == summarizedweights[i]
             end
         end
 
+        @testset "Triangular: Alternative: 2, Criteria: 4" begin
+
+            eps = 0.01
+
+            decmat = [
+                Triangular(3, 5, 7) Triangular(7, 9, 9) Triangular(1, 4, 7) Triangular(3, 5, 7)
+                Triangular(5, 7, 9) Triangular(5, 8, 9) Triangular(1, 3, 5) Triangular(1, 3, 5)
+            ]
+
+            n, p = size(decmat)
+
+            w = [
+                Triangular(3, 6, 9),
+                Triangular(5, 8, 9),
+                Triangular(5, 8, 9),
+                Triangular(1, 4, 7),
+            ]
+
+            fns = [minimum, maximum, maximum, maximum]
+
+            result = fuzzytopsis(decmat, w, fns)
+
+            expected_normalized_mat = [
+                Triangular(0.429, 0.600, 1.000) Triangular(0.778, 1.000, 1.000) Triangular(0.143, 0.571, 1.000) Triangular(0.429, 0.714, 1.000)
+                Triangular(0.333, 0.429, 0.600) Triangular(0.556, 0.889, 1.000) Triangular(0.143, 0.429, 0.714) Triangular(0.143, 0.429, 0.714)
+            ]
+
+            for i = 1:n
+                for j = 1:p
+                    @test isapprox(
+                        result.normalized_decmat[i, j],
+                        expected_normalized_mat[i, j],
+                        atol=eps,
+                    )
+                end
+            end
+
+            expected_weighted_norm = [
+                Triangular(1.287, 3.600, 9.000) Triangular(3.890, 8.000, 9.000) Triangular(0.715, 4.568, 9.000) Triangular(0.429, 2.856, 7.000)
+                Triangular(0.999, 2.574, 5.400) Triangular(2.780, 7.112, 9.000) Triangular(0.715, 3.432, 6.426) Triangular(0.143, 1.716, 4.998)
+            ]
+
+            for i = 1:n
+                for j = 1:p
+                    @test isapprox(
+                        result.weighted_normalized_decmat[i, j],
+                        expected_weighted_norm[i, j],
+                        atol=eps,
+                    )
+                end
+            end
 
 
-        @test result.bestideal == [
-            Triangular(9.0, 9.0, 9.0),
-            Triangular(9.0, 9.0, 9.0),
-            Triangular(9.0, 9.0, 9.0),
-            Triangular(7.0, 7.0, 7.0),
-        ]
 
-        @test result.worstideal == [
-            Triangular(1.0, 1.0, 1.0),
-            Triangular(2.7777777777777777, 2.7777777777777777, 2.7777777777777777),
-            Triangular(0.7142857142857142, 0.7142857142857142, 0.7142857142857142),
-            Triangular(0.14285714285714285, 0.14285714285714285, 0.14285714285714285),
-        ]
+            @test result.bestideal == [
+                Triangular(9.0, 9.0, 9.0),
+                Triangular(9.0, 9.0, 9.0),
+                Triangular(9.0, 9.0, 9.0),
+                Triangular(7.0, 7.0, 7.0),
+            ]
 
-        @test result.sminus == [19.130795215674166, 13.675031586664522]
+            @test result.worstideal == [
+                Triangular(1.0, 1.0, 1.0),
+                Triangular(2.7777777777777777, 2.7777777777777777, 2.7777777777777777),
+                Triangular(0.7142857142857142, 0.7142857142857142, 0.7142857142857142),
+                Triangular(0.14285714285714285, 0.14285714285714285, 0.14285714285714285),
+            ]
 
-        @test result.splus == [18.352692130107226, 21.116560843933744]
+            @test result.sminus == [19.130795215674166, 13.675031586664522]
+
+            @test result.splus == [18.352692130107226, 21.116560843933744]
 
 
-        @test isapprox(result.scores[1], 0.51, atol=eps)
-        @test isapprox(result.scores[2], 0.39, atol=eps)
+            @test isapprox(result.scores[1], 0.51, atol=eps)
+            @test isapprox(result.scores[2], 0.39, atol=eps)
 
+        end
     end
-
 
 
     @testset "A full example" begin
@@ -190,21 +200,21 @@
         fns = [maximum for i in 1:5]
         result = fuzzytopsis(decmat, weights, fns)
 
-        myorder = result.scores |> sortperm 
+        myorder = result.scores |> sortperm
 
         @test myorder[1] == 5
         @test myorder[2] == 3
-        @test myorder[3] == 4 
+        @test myorder[3] == 4
         @test myorder[4] == 2
         @test myorder[5] == 1
 
-        
-    
-        @test isapprox(result.scores[1], 0.611, atol = atol)
-        @test isapprox(result.scores[2], 0.540, atol = atol)
-        @test isapprox(result.scores[3], 0.48, atol = atol)
-        @test isapprox(result.scores[4], 0.505, atol = atol)
-        @test isapprox(result.scores[5], 0.350, atol = atol)
-        
+
+
+        @test isapprox(result.scores[1], 0.611, atol=atol)
+        @test isapprox(result.scores[2], 0.540, atol=atol)
+        @test isapprox(result.scores[3], 0.48, atol=atol)
+        @test isapprox(result.scores[4], 0.505, atol=atol)
+        @test isapprox(result.scores[5], 0.350, atol=atol)
+
     end
 end
