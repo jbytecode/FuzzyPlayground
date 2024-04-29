5712486a56124327967e1b767ca03d1fdfbfada5
mhsatman@gmail.com <mhsatman@gmail.com>
2024-04-29 10:30:58 +0300

    reformat edas

--- src/edas.jl
+++ src/edas.jl
@@ -6,7 +6,7 @@ mutable struct FuzzyEdasResult
     avgdefuz::Vector
     pda::Matrix
     nda::Matrix
-    wpda::Matrix 
+    wpda::Matrix
     wnda::Matrix
     sp::Vector
     sn::Vector
@@ -42,7 +42,7 @@ function fuzzyedas(decmat::Matrix{Triangular}, w::Vector{Triangular}, fns)
     wpda = zeros(Triangular, n, p)
     wnda = zeros(Triangular, n, p)
     sp = zeros(Triangular, n)
-    sn = zeros(Triangular, n) 
+    sn = zeros(Triangular, n)
     sp_defuz = zeros(Float64, n)
     sn_defuz = zeros(Float64, n)
     nsp = zeros(Triangular, n)
@@ -50,7 +50,7 @@ function fuzzyedas(decmat::Matrix{Triangular}, w::Vector{Triangular}, fns)
     as = zeros(Triangular, n)
     scores = zeros(Float64, n)
     ranks = zeros(Int64, n)
-    
+
     for i in 1:n
         for j in 1:p
             amean = map(x -> x.a, decmat[:, j]) |> Statistics.mean
@@ -70,7 +70,7 @@ function fuzzyedas(decmat::Matrix{Triangular}, w::Vector{Triangular}, fns)
                     nda[i, j] = Triangular(sort([a, b, c]))
                     pda[i, j] = Triangular(0, 0, 0)
                 end
-            elseif fns[j] == minimum 
+            elseif fns[j] == minimum
                 if avgdefuz[j] <= defuzmatrix[i, j]
                     a = (decmat[i, j].a - cmean) / avgdefuz[j]
                     b = (decmat[i, j].b - bmean) / avgdefuz[j]
@@ -83,7 +83,7 @@ function fuzzyedas(decmat::Matrix{Triangular}, w::Vector{Triangular}, fns)
                     c = (cmean - decmat[i, j].a) / avgdefuz[j]
                     pda[i, j] = Triangular(sort([a, b, c]))
                     nda[i, j] = Triangular(0, 0, 0)
-                end 
+                end
             else
                 error("Unsupported function: $(fns[p])")
             end
@@ -95,8 +95,8 @@ function fuzzyedas(decmat::Matrix{Triangular}, w::Vector{Triangular}, fns)
         for j in 1:p
             wpda[i, j] = Triangular(w[j].a * pda[i, j].a, w[j].b * pda[i, j].b, w[j].c * pda[i, j].c)
             wnda[i, j] = Triangular(w[j].a * nda[i, j].a, w[j].b * nda[i, j].b, w[j].c * nda[i, j].c)
-        end 
-    end 
+        end
+    end
 
     # SP and SN 
     for i in 1:n
@@ -109,35 +109,35 @@ function fuzzyedas(decmat::Matrix{Triangular}, w::Vector{Triangular}, fns)
         b = sum(map(x -> x.b, wnda[i, :]))
         c = sum(map(x -> x.c, wnda[i, :]))
         sn[i] = Triangular(a, b, c)
-    end 
+    end
 
     # SP defuz and NP defuz
-    for i in 1:n 
+    for i in 1:n
         sp_defuz[i] = (sp[i].a + sp[i].b + sp[i].c) / 3
-        sn_defuz[i] = (sn[i].a + sn[i].b + sn[i].c) / 3 
-    end 
+        sn_defuz[i] = (sn[i].a + sn[i].b + sn[i].c) / 3
+    end
 
     # NSP and NSN 
     max_sp_defuz = maximum(sp_defuz)
     max_sn_defuz = maximum(sn_defuz)
-    for i in 1:n 
-        nsp[i] = Triangular(sp[i].a / max_sp_defuz, sp[i].b/max_sp_defuz, sp[i].c/max_sp_defuz)
+    for i in 1:n
+        nsp[i] = Triangular(sp[i].a / max_sp_defuz, sp[i].b / max_sp_defuz, sp[i].c / max_sp_defuz)
         nsn[i] = Triangular(1 - (sn[i].c / max_sn_defuz), 1 - (sn[i].b / max_sn_defuz), 1 - (sn[i].a / max_sn_defuz))
-        as[i]  = Triangular(0.5*(nsp[i].a + nsn[i].a), 0.5*(nsp[i].b + nsn[i].b), 0.5*(nsp[i].c + nsn[i].c))
-    end 
+        as[i] = Triangular(0.5 * (nsp[i].a + nsn[i].a), 0.5 * (nsp[i].b + nsn[i].b), 0.5 * (nsp[i].c + nsn[i].c))
+    end
 
     # Scores are Defuz of AS 
     for i in 1:n
         scores[i] = (as[i].a + as[i].b + as[i].c) / 3
-    end 
+    end
 
 
-    ranks = (sortperm(scores, rev = true) |> invperm)
+    ranks = (sortperm(scores, rev=true) |> invperm)
 
-    
 
-    edasresult = FuzzyEdasResult(w, decmat, fns, defuzmatrix, avgdefuz, 
-                pda, nda, wpda, wnda,sp,sn, sp_defuz, sn_defuz, nsp, nsn, as, scores, ranks)
+
+    edasresult = FuzzyEdasResult(w, decmat, fns, defuzmatrix, avgdefuz,
+        pda, nda, wpda, wnda, sp, sn, sp_defuz, sn_defuz, nsp, nsn, as, scores, ranks)
 
     return edasresult
 end
